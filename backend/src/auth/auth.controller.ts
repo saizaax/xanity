55ad69f581db23c9e67045ea004cf06b7f6ca1f1
saizaax <flamesuspect@gmail.com>
2022-05-30 15:33:12 +0300

    Added cors & preauthorize controller.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -10,6 +10,11 @@ import { UsersService } from "src/users/users.service"
 import { AuthService } from "./auth.service"
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger"
 import { TokenResponseDto } from "./dto/token-response.dto"
+import { UserDto } from "src/users/dto/user.dto"
+import { User } from "src/users/user.decorator"
+import { JwtAuthGuard } from "./guards/jwt.guard"
+import { UseGuards } from "@nestjs/common"
+import { Get } from "@nestjs/common"
 
 @ApiTags("Auth")
 @Controller("auth")
@@ -39,5 +44,13 @@ export class AuthController {
       throw new HttpException("User already exists", HttpStatus.BAD_REQUEST)
 
     return this.usersService.create(body)
+  }
+
+  @ApiOperation({ summary: "Check for JWT token and return user data" })
+  @ApiResponse({ status: 200, type: UserDto })
+  @UseGuards(JwtAuthGuard)
+  @Get("preauthorize")
+  async updateFavorite(@User() user: UserDto) {
+    return user
   }
 }
